714ac30f5bd7b8c1c37491178a89bc9145dde5ca
“Mett-Barrgit config --global user.name “Mett-Barr <ohmylove347@gmail.comgit config --global user.name Mett-Barr>
2021-09-14 18:07:42 +0800

    snackbar 01

--- app/src/main/java/com/example/allinone/page2/components/ComponentsFragment.kt
+++ app/src/main/java/com/example/allinone/page2/components/ComponentsFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.example.allinone.R
 import com.example.allinone.databinding.FragmentComponentsBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.SnackbarLayout
 
 class ComponentsFragment : Fragment() {
     private lateinit var binding: FragmentComponentsBinding
@@ -27,9 +31,39 @@ class ComponentsFragment : Fragment() {
     }
 
     private fun init() {
+        slider()
+        snackbarInit()
+    }
+
+    private fun slider() {
         binding.slider.addOnChangeListener { _, value, _ ->
             viewModel.setSliderNum(value)
             binding.textView6.text = String.format("%.2f", value)
+        }
+    }
+
+    private fun snackbarInit() {
+//        binding.button9.setOnClickListener {
+//            val snackbar = Snackbar.make(binding.Container, "Basic", Snackbar.LENGTH_SHORT)
+//            snackbar.setAction("Dismiss") {
+//                snackbar.dismiss()
+//            }
+//            snackbar.show()
+//        }
+
+        binding.button8.setOnClickListener {
+            val snackbar = Snackbar.make(binding.Container, "Work!", Snackbar.LENGTH_SHORT)
+            snackbar.setAction("Toast") {
+                snackbar.dismiss()
+                Toast.makeText(activity, "Toast!", Toast.LENGTH_SHORT).show()
+            }
+
+//            val layout = snackbar.view as SnackbarLayout
+//            val textView: TextView =
+//                layout.findViewById<View>(android.support.design.R.id.snackbar_text) as TextView
+//            textView.visibility = View.INVISIBLE
+
+            snackbar.show()
         }
     }
 
